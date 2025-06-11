3e0f483e415ac6c93062898ab75adce4665960fd
William Huang <103707873+williamhuang3@users.noreply.github.com>
2025-06-11 00:02:15 -0700

    hi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
